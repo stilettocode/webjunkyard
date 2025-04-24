7b44d2591da1024f766bacb14f8cdc150da01f64
Geraldo Cisneros <geraldo.a.cisneros@nasa.gov>
2025-04-24 17:44:56 -0500

    :bug: fix ltv ping sending on page load

--- public/javascript/rover.js
+++ public/javascript/rover.js
@@ -14,30 +14,36 @@ function loadVideoFeed() {
     video.src = "http://192.168.51.163:8080/stream?topic=/camera/image_raw&type=ros_compressed";
 }
 
-function loadPOI(){
-
+function loadPOI() {
     fetch("../server_ip.txt")
     .then(response => response.text())
-    .then(ip => fetch(`http://${ip}:14141`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "text/plain",
-            "User-Agent": "rover_" 
-        },
-        body: "rover_ping=true" 
-    }))
-    .then(response => response.text()) 
-    .then(data => console.log("Success:", data))
-    .catch(error => console.error("Error:", error));
-   
-    $.getJSON("../json_data/ROVER.json", function(data) {
-        poi1x.innerText = (data.rover.poi_1_x).toFixed(5);
-        poi2x.innerText = (data.rover.poi_2_x).toFixed(5);
-        poi3x.innerText = (data.rover.poi_3_x).toFixed(5);
-        poi1y.innerText = (data.rover.poi_1_y).toFixed(5);
-        poi2y.innerText = (data.rover.poi_2_y).toFixed(5);
-        poi3y.innerText = (data.rover.poi_3_y).toFixed(5);
+    .then(ip => {
+        return fetch(`http://${ip}:14141`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "text/plain",
+                "User-Agent": "rover_"
+            },
+            body: "rover_ping=true"
+        });
     })
+    .then(response => response.text())
+    .then(data => {
+        $.getJSON("../json_data/ROVER.json", function(data) {
+            poi1x.innerText = (data.rover.poi_1_x).toFixed(5);
+            poi2x.innerText = (data.rover.poi_2_x).toFixed(5);
+            poi3x.innerText = (data.rover.poi_3_x).toFixed(5);
+            poi1y.innerText = (data.rover.poi_1_y).toFixed(5);
+            poi2y.innerText = (data.rover.poi_2_y).toFixed(5);
+            poi3y.innerText = (data.rover.poi_3_y).toFixed(5);
+        });
+    })
+    .catch(error => console.error("Error:", error));
+}
+
+// Loads the rock yard maps
+function loadMap() {
+    map.src = "./images/rockYardMap-min.png";
 }
 
 // Loads the rock yard maps
@@ -59,8 +65,16 @@ function onload() {
     poi3x = document.getElementById('poi3x');
     poi3y = document.getElementById('poi3y');
 
+    $.getJSON("../json_data/ROVER.json", function(data) {
+        poi1x.innerText = (data.rover.poi_1_x).toFixed(5);
+        poi2x.innerText = (data.rover.poi_2_x).toFixed(5);
+        poi3x.innerText = (data.rover.poi_3_x).toFixed(5);
+        poi1y.innerText = (data.rover.poi_1_y).toFixed(5);
+        poi2y.innerText = (data.rover.poi_2_y).toFixed(5);
+        poi3y.innerText = (data.rover.poi_3_y).toFixed(5);
+    });
+
     loadMap();
-    loadPOI();
 
     // Continuously refreshes for the video feed
     setInterval(function() {
